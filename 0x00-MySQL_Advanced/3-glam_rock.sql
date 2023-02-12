7baf823bd60915ed7a3fa7fa7b304dd28fb8ea16
Timothy Atobatele <oladimeji.atobatele@gmail.com>
2023-02-12 00:55:07 +0000

    update

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
-cript that lists all bands with Glam rock as their main style, ranked by their longevity
+--script that lists all bands with Glam rock as their main style, ranked by their longevity
 
 SELECT
     band_name, (IFNULL(split, 2022) - formed) AS lifespan
